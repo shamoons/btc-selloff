784c987fdb9d88df809237396afb0682fadaedc1
Shamoon Siddiqui <shamoon@develop.io>
2020-03-10 12:05:49 -0400

    Trying a different selection function

--- simulate.py
+++ simulate.py
@@ -1,6 +1,10 @@
 import argparse
 import pandas as pd
 import numpy as np
+import wandb
+import os.path as path
+import math
+from hyperopt import hp, fmin, tpe
 
 
 def parse_args():
@@ -16,13 +20,181 @@ def parse_args():
                         type=float, required=True)
     parser.add_argument('--coefficients', help='Coefficients',
                         type=float, nargs=3)
+    parser.add_argument('--max_evals', help='Max Evals for param search',
+                        type=int)
 
     args = parser.parse_args()
 
     return args
 
 
+def objective(args, evaluation=False, log_run=True):
+    # print('c1: {}\tc2: {}\tc3: {}'.format(args['c1'], args['c2'], args['c3']))
+    # if args['c1'] == 0 or args['c2'] == 0 or args['c3'] == 0:
+    #     return 1e10
+    btc_usd_frame = args['btc_usd_frame']
+    results_df = pd.DataFrame(columns=[
+                              'Run', 'Current BTC', 'Total Profit', 'Average BTC Price', 'Ending BTC Price'])
+    for run in range(args['num_runs']):
+
+        start_index = np.random.randint(
+            0, len(btc_usd_frame) - args['num_weeks'] * 7)
+        end_index = start_index + args['num_weeks'] * 7
+
+        sample_frame = btc_usd_frame[start_index:end_index]
+        current_price = args['current_price']
+        total_profit = 0.0
+
+        current_btc_amount = args['total_btc']
+        weeks = 0
+        average_btc_price = 0
+        cnt = 0
+        for _, row in sample_frame.iterrows():
+            cnt += 1
+            percent_change = float(row['Perc Close'].strip('%')) / 100
+            current_price = current_price + current_price * percent_change
+            average_btc_price += current_price
+
+            if cnt % 7 != 0:
+                continue
+            weeks += 1
+            # price_to_sell = args['c1'] * current_price ** 2 + \
+            #     args['c2'] * current_price + args['c3']
+
+            btc_to_sell = args['c1'] + args['c2'] * \
+                math.exp(current_price * args['c3'])
+
+            # btc_to_sell = price_to_sell / current_price
+
+            # if current_btc_amount < btc_to_sell or price_to_sell <= 0:
+            if current_btc_amount < btc_to_sell:
+                continue
+
+            current_btc_amount = current_btc_amount - btc_to_sell
+            total_profit = total_profit + btc_to_sell * current_price
+
+            if args['num_runs'] == 1:
+                print('\t', _)
+                print('\tPercent Change: {:.4f}'.format(percent_change))
+                print('\tCurrent price: {:.2f}'.format(current_price))
+                print('\tAverage price: {:.2f}'.format(
+                    average_btc_price / cnt))
+
+                print('\t\tPrice to sell: {:.2f}'.format(price_to_sell))
+                print(
+                    '\t\tBTC to sell: {:.4f} / {:.4f}'.format(btc_to_sell, current_btc_amount))
+                print('\t\tTotal profit: {:.2f}'.format(total_profit))
+
+        average_btc_price /= cnt
+        results_df = results_df.append({'Run': run, 'Current BTC': current_btc_amount,
+                                        'Total Profit': total_profit, 'Ending BTC Price': current_price, 'Average BTC Price': average_btc_price}, ignore_index=True)
+
+    if results_df['Total Profit'].min() < 1000 or results_df['Total Profit'].std() < 1000 or results_df['Total Profit'].max() < 1000:
+        score = 1e10
+    else:
+        score = results_df['Total Profit'].std(
+        ) / (results_df['Total Profit'].min())
+
+    summary = {
+        'Mean': results_df['Total Profit'].mean(),
+        'STD': results_df['Total Profit'].std(),
+        'Median': results_df['Total Profit'].median(),
+        'Min': results_df['Total Profit'].min(),
+        'Max': results_df['Total Profit'].max(),
+        'Range': results_df['Total Profit'].max() - results_df['Total Profit'].min(),
+        'Average BTC Price': average_btc_price,
+        'Ending BTC Price': current_price,
+        'score': score,
+        'c1': args['c1'],
+        'c2': args['c2'],
+        'c3': args['c3']
+    }
+
+    if evaluation == True:
+        results_df.to_csv('results.csv')
+        print(summary)
+        # wandb.run.summary = summary
+        # wandb.run.summary.update()
+        # print(results_df)
+        print('Saved to: results.csv')
+
+    if log_run == True:
+        summary_df = pd.DataFrame()
+        summary_df = summary_df.append(summary, ignore_index=True)
+        wandb.log(summary)
+
+        summary_df.to_csv(path.join(wandb.run.dir, 'coefficients.csv'))
+
+    return score
+
+
+def get_space(args, btc_usd_frame, init_c1, init_c2, init_c3):
+    if args.coefficients:
+        c1 = args.coefficients[0]
+        c2 = args.coefficients[1]
+        c3 = args.coefficients[2]
+    else:
+        c1 = hp.normal('c1', init_c1['mean'], init_c1['sigma'])
+        c2 = hp.normal('c2', init_c2['mean'], init_c2['sigma'])
+        c3 = hp.normal('c3', init_c3['mean'], init_c3['sigma'])
+
+    return {
+        'c1': c1,
+        'c2': c2,
+        'c3': c3,
+        'current_price': args.current_price,
+        'total_btc': args.total_btc,
+        'num_runs': args.num_runs,
+        'num_weeks': args.num_weeks,
+        'btc_usd_frame': btc_usd_frame
+    }
+
+
 def main():
+    args = parse_args()
+    print('Starting.... {}\n'.format(args.current_price))
+    evaluation = False
+
+    if args.coefficients:
+        evaluation = True
+        log_run = False
+
+    btc_usd_frame = pd.read_csv('BTC-USD.csv')
+
+    init_c1 = {'mean': 0, 'sigma': 100}
+    init_c2 = {'mean': 0.005, 'sigma': 0.004}
+    init_c3 = {'mean': 0.00002, 'sigma': 0.000001}
+    space = get_space(args, btc_usd_frame, init_c1, init_c2, init_c3)
+
+    if evaluation == False:
+        wandb.init(project="btc-sell", config={
+            "Max Eval": args.max_evals,
+            "Num Runs": args.num_runs,
+            "Total BTC": args.total_btc,
+            "Current Price": args.current_price,
+            'C1_mean': init_c1['mean'],
+            'C1_sigma': init_c1['sigma'],
+            'C2_mean': init_c2['mean'],
+            'C2_sigma': init_c2['sigma'],
+            'C3_mean': init_c3['mean'],
+            'C3_sigma': init_c3['sigma']
+        })
+
+        best = fmin(objective,
+                    space=space,
+                    algo=tpe.suggest,
+                    max_evals=args.max_evals)
+        print('Best Summary: ', best)
+        space['c1'] = best['c1']
+        space['c2'] = best['c2']
+        space['c3'] = best['c3']
+        log_run = True
+
+    eval_score = objective(space, evaluation=True, log_run=log_run)
+    print('Evaluation Score: ', eval_score)
+
+
+def main2():
     args = parse_args()
     param_search = True
 
@@ -57,51 +229,6 @@ def main():
         c_index += 1
         results_df = pd.DataFrame(
             columns=['Run', 'Current BTC', 'Total Profit'])
-        for run in range(args.num_runs):
-
-            start_index = np.random.randint(
-                0, len(btc_usd_frame) - args.num_weeks * 7)
-            end_index = start_index + args.num_weeks * 7
-
-            sample_frame = btc_usd_frame[start_index:end_index]
-            current_price = args.current_price
-            total_profit = 0.0
-
-            current_btc_amount = args.total_btc
-            weeks = 0
-            average_btc_price = 0
-            cnt = 0
-            for index, row in sample_frame.iterrows():
-                cnt += 1
-                percent_change = float(row['Perc Close'].strip('%')) / 100
-                current_price = current_price + current_price * percent_change
-                average_btc_price += current_price
-
-                if cnt % 7 != 0:
-                    continue
-                weeks += 1
-                price_to_sell = coefficient[0] * current_price ** 2 + \
-                    coefficient[1] * current_price + coefficient[2]
-
-                btc_to_sell = price_to_sell / current_price
-
-                if current_btc_amount <= btc_to_sell or price_to_sell <= 0:
-                    continue
-
-                current_btc_amount = current_btc_amount - btc_to_sell
-                total_profit = total_profit + btc_to_sell * current_price
-
-                if param_search == False and args.num_runs == 1:
-                    print('\t', index)
-                    print('\tCurrent price: {:.2f}'.format(current_price))
-                    print('\t\tPrice to sell: {:.2f}'.format(price_to_sell))
-                    print(
-                        '\t\tBTC to sell: {:.2f} / {:.2f}'.format(btc_to_sell, current_btc_amount))
-                    print('\t\tTotal profit: {:.2f}'.format(total_profit))
-
-            average_btc_price /= len(sample_frame)
-            results_df = results_df.append({'Run': run, 'Current BTC': current_btc_amount,
-                                            'Total Profit': total_profit, 'Ending BTC Price': current_price, 'Average BTC Price': average_btc_price}, ignore_index=True)
 
         print(c_index, '/', len(coefficients))
         summary_df = pd.DataFrame(
